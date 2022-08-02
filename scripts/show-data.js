3b0164be1ecba204dbee48eba4d7141986c77584
Luan Messias <luan44058@gmail.com>
2022-08-02 18:12:15 -0300

    Desafio 1 - Concluído

---
+++ scripts/show-data.js
@@ -0,0 +1,7 @@
+function showData() {
+    var name = document.getElementById('name').value
+    console.log(name)
+
+    var birthday = document.getElementById('birthday').value
+    console.log(birthday)
+}
